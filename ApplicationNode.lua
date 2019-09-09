603ad15e4760570c5f52176b7ec9eb2ccc9a56e4
majdkassawat <majd.kassawat@gmail.com>
2019-09-09 13:58:13 +0200

    Frames management and simple example: - Added FramesTools.lua file to manage frames and transformations - A simple example to print the example camera frame updating z position

--- ApplicationNode.lua
+++ ApplicationNode.lua
@@ -1,8 +1,8 @@
-local api = require("BuilderBotAPI")
-local bt = require("luabt")
+local api = require('BuilderBotAPI')
+local bt = require('luabt')
 local app_node = {}
 
-app_node.grap_block = require("grap_block")
+app_node.grap_block = require('grap_block')
 --app_node.approach = require("approach") -- TODO
 
 return app_node
